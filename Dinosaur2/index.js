b189740cbe2f8349af169fa716d710993fc8e263
chenxu <chenxu.ios93@gmail.com>
2019-10-14 17:10:51 +0800

    no message

--- Dinosaur2/index.js
+++
@@ -1,1 +0,0 @@
-window.screenOrientation="portrait",loadLib("libs/laya.core.js"),loadLib("libs/laya.html.js"),loadLib("libs/laya.ui.js"),loadLib("libs/laya.d3.js"),loadLib("libs/laya.physics3D.js"),loadLib("js/bundle.js");
